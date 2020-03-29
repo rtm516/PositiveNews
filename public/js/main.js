7fefe08433fcbd1a91d473fac0b5d3b32ca5a318
rtm516 <ryantmilner@hotmail.co.uk>
2020-03-29 00:54:26 +0000

    Added date display

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,39 @@
+function formatDate (date) {
+  var d = new Date(date)
+  var month = '' + (d.getMonth() + 1)
+  var day = '' + d.getDate()
+  var year = d.getFullYear()
+
+  if (month.length < 2) { month = '0' + month }
+  if (day.length < 2) { day = '0' + day }
+
+  return formatAMPM(d) + ' ' + [day, month, year].join('/')
+}
+
+function formatAMPM (date) {
+  var hours = date.getHours()
+  var minutes = date.getMinutes()
+  var ampm = hours >= 12 ? 'PM' : 'AM'
+
+  hours = hours % 12
+  hours = hours || 12
+  minutes = minutes < 10 ? '0' + minutes : minutes
+
+  var strTime = hours + ':' + minutes + ampm
+  return strTime
+}
+
 function generateArticle (article) {
   let newHTML = ''
+
+  article.date = formatDate(article.date)
 
   newHTML += '<div class="card">'
 
   newHTML += '<div class="card-body">'
 
   newHTML += `<h5 class="card-title">${article.title}</h5>`
+  newHTML += `<h6 class="card-subtitle mb-2 text-muted">${article.date}</h6>`
   newHTML += `<p class="card-text">${article.desc}</p>`
   newHTML += `<a href="${article.url}" class="card-link">Visit</a>`
 
