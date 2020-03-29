f86bbfb37f7b66647225f1c02b1a43a95f159b7a
rtm516 <ryantmilner@hotmail.co.uk>
2020-03-29 21:22:38 +0100

    Removed spell check remnants

--- nlp.js
+++ nlp.js
@@ -1,12 +1,8 @@
 // Courtesy of Ebenezer Don (2020) https://blog.logrocket.com/sentiment-analysis-node-js/
 
 const { WordTokenizer, SentimentAnalyzer, PorterStemmer } = require('natural')
-const SpellCorrector = require('spelling-corrector')
 const aposToLexForm = require('apos-to-lex-form')
 const SW = require('stopword')
-
-const spellCorrector = new SpellCorrector()
-spellCorrector.loadDictionary()
 
 module.exports.getSentiment = function (text) {
   // Pre-processing
@@ -17,13 +13,6 @@ module.exports.getSentiment = function (text) {
   // Tokenisation
   const tokenizer = new WordTokenizer()
   const tokenizedText = tokenizer.tokenize(alphaOnlyText)
-
-  // Spelling corrections - removed to test speed implications
-  /*
-  tokenizedText.forEach((word, index) => {
-    tokenizedText[index] = spellCorrector.correct(word)
-  })
-  */
 
   // Remove stopwords (but, a, or, what)
   const filteredText = SW.removeStopwords(tokenizedText)
